275697daf0b5a39faa155674cad213e5c7c57d1a
Stefan Froemken <froemken@gmail.com>
2017-05-04 14:27:09 +0200

    Update for news 5 >

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,11 +27,11 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-7.99.99',
-            'news' => '4.0.0-4.99.99'
+            'typo3' => '7.6.0-8.99.99',
+            'news' => '5.0.0-6.99.99'
         ),
         'conflicts' => array(
         ),
